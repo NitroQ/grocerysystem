a689037bc6d769a7e9eb713df3836d3a268566d6
thejldeleon <deleon.jlouie@gmail.com>
2022-03-07 16:58:37 +0800

    Initial Commit

---
+++ src/GoShopper_Admin2.java
@@ -0,0 +1,141 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Component;
+import javax.swing.table.TableModel;
+
+public class GoShopper_Admin2 {
+
+	private JFrame frame;
+	private JTable table_EmployeeList;
+	private JScrollPane scrollPane;
+    private  String[] columns = {"Employee Number", "Employee Name", "Role", "Hours", "DTR"};
+    private Object[][] data = {};
+     @SuppressWarnings("serial")
+     private DefaultTableModel model = new DefaultTableModel(data, columns) {
+         @Override
+           public boolean isCellEditable(int row, int column) {
+               return false;
+           }
+     };
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GoShopper_Admin2 window = new GoShopper_Admin2();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GoShopper_Admin2() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setBounds(100, 100, 1020, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton LogOut = new JButton("Log-Out");
+		LogOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		LogOut.setForeground(Color.WHITE);
+		LogOut.setFont(new Font("Roboto", Font.BOLD, 14));
+		LogOut.setBackground(new Color(47, 79, 79));
+		LogOut.setBounds(865, 34, 120, 35);
+		frame.getContentPane().add(LogOut);
+		
+		JLabel lblNewLabel_1 = new JLabel("HRIS Records");
+		lblNewLabel_1.setForeground(Color.BLACK);
+		lblNewLabel_1.setFont(new Font("Roboto", Font.BOLD, 39));
+		lblNewLabel_1.setBounds(35, 79, 331, 52);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(192, 192, 192), 3, true));
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(35, 137, 950, 499);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		table_EmployeeList = new JTable(model);
+		table_EmployeeList.setFont(new Font("Roboto", Font.PLAIN, 17));
+		table_EmployeeList.setBackground(Color.WHITE);
+		table_EmployeeList.setBorder(null);
+		table_EmployeeList.setRowHeight(30);
+		table_EmployeeList.setBounds(24, 174, 956, 329);
+		JScrollPane scrollPane_1 = new JScrollPane(table_EmployeeList);
+		scrollPane_1.setBounds(23, 84, 893, 392);
+		panel.add(scrollPane_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Employee List");
+		lblNewLabel_1_1.setBounds(23, 27, 331, 38);
+		panel.add(lblNewLabel_1_1);
+		lblNewLabel_1_1.setForeground(Color.BLACK);
+		lblNewLabel_1_1.setFont(new Font("Roboto", Font.BOLD, 30));
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setForeground(Color.WHITE);
+		btnAdd.setFont(new Font("Roboto", Font.BOLD, 14));
+		btnAdd.setBackground(new Color(204, 102, 102));
+		btnAdd.setBounds(635, 30, 78, 35);
+		panel.add(btnAdd);
+		
+		JButton btnEdit = new JButton("Edit");
+		btnEdit.setForeground(Color.WHITE);
+		btnEdit.setFont(new Font("Roboto", Font.BOLD, 14));
+		btnEdit.setBackground(new Color(204, 102, 102));
+		btnEdit.setBounds(725, 30, 80, 35);
+		panel.add(btnEdit);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setForeground(Color.WHITE);
+		btnDelete.setFont(new Font("Roboto", Font.BOLD, 14));
+		btnDelete.setBackground(new Color(204, 102, 102));
+		btnDelete.setBounds(814, 30, 102, 35);
+		panel.add(btnDelete);
+		
+		JLabel GoShopperAdmin_Logo = new JLabel("New label");
+		GoShopperAdmin_Logo.setIcon(new ImageIcon(GoShopper_Admin2.class.getResource("/GoShopper_AdminAssets/LogoAdmin.png")));
+		GoShopperAdmin_Logo.setBounds(0, 0, 1006, 685);
+		frame.getContentPane().add(GoShopperAdmin_Logo);
+		
+		JLabel GoShopperAdmin_Logo_1 = new JLabel("New label");
+		GoShopperAdmin_Logo_1.setIcon(new ImageIcon(GoShopper_Admin2.class.getResource("/GoShopper_AdminAssets/BGAdmin.png")));
+		GoShopperAdmin_Logo_1.setBounds(0, 0, 1006, 685);
+		frame.getContentPane().add(GoShopperAdmin_Logo_1);
+	}
+}
---
+++ src/GoShopper_AdminEdit.java
@@ -0,0 +1,139 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Component;
+import javax.swing.table.TableModel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+
+public class GoShopper_AdminEdit {
+
+	private JFrame frame;
+	private JScrollPane scrollPane;
+    private  String[] columns = {"Employee Number", "Employee Name", "Role", "Hours", "DTR"};
+    private Object[][] data = {};
+     @SuppressWarnings("serial")
+     private DefaultTableModel model = new DefaultTableModel(data, columns) {
+         @Override
+           public boolean isCellEditable(int row, int column) {
+               return false;
+           }
+     };
+     private JTextField field_EmpName;
+     private JTextField field_EmpNumber;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GoShopper_AdminEdit window = new GoShopper_AdminEdit();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GoShopper_AdminEdit() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setBounds(100, 100, 367, 430);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Edit Employee");
+		lblNewLabel_1.setForeground(Color.BLACK);
+		lblNewLabel_1.setFont(new Font("Roboto", Font.BOLD, 20));
+		lblNewLabel_1.setBounds(28, 85, 325, 35);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Employee Name");
+		lblNewLabel_1_1.setForeground(Color.BLACK);
+		lblNewLabel_1_1.setFont(new Font("Roboto", Font.ITALIC, 16));
+		lblNewLabel_1_1.setBounds(28, 119, 325, 29);
+		frame.getContentPane().add(lblNewLabel_1_1);
+		
+		field_EmpName = new JTextField();
+		field_EmpName.setFont(new Font("Roboto", Font.PLAIN, 12));
+		field_EmpName.setBackground(new Color(220, 220, 220));
+		field_EmpName.setBounds(28, 150, 294, 29);
+		frame.getContentPane().add(field_EmpName);
+		field_EmpName.setColumns(10);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Employee Number");
+		lblNewLabel_1_1_1.setForeground(Color.BLACK);
+		lblNewLabel_1_1_1.setFont(new Font("Roboto", Font.ITALIC, 16));
+		lblNewLabel_1_1_1.setBounds(28, 185, 325, 29);
+		frame.getContentPane().add(lblNewLabel_1_1_1);
+		
+		field_EmpNumber = new JTextField();
+		field_EmpNumber.setFont(new Font("Roboto", Font.PLAIN, 12));
+		field_EmpNumber.setColumns(10);
+		field_EmpNumber.setBackground(new Color(220, 220, 220));
+		field_EmpNumber.setBounds(28, 218, 294, 29);
+		frame.getContentPane().add(field_EmpNumber);
+		
+		JLabel lblNewLabel_1_1_1_1 = new JLabel("Role");
+		lblNewLabel_1_1_1_1.setForeground(Color.BLACK);
+		lblNewLabel_1_1_1_1.setFont(new Font("Roboto", Font.ITALIC, 16));
+		lblNewLabel_1_1_1_1.setBounds(28, 252, 325, 29);
+		frame.getContentPane().add(lblNewLabel_1_1_1_1);
+		
+		JComboBox combo_Role = new JComboBox();
+		combo_Role.setBackground(new Color(220, 220, 220));
+		combo_Role.setBounds(28, 280, 294, 29);
+		frame.getContentPane().add(combo_Role);
+		
+		JButton btnEdit = new JButton("Save");
+		btnEdit.setForeground(Color.WHITE);
+		btnEdit.setFont(new Font("Roboto", Font.BOLD, 14));
+		btnEdit.setBackground(new Color(204, 102, 102));
+		btnEdit.setBounds(28, 330, 78, 35);
+		frame.getContentPane().add(btnEdit);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCancel.setForeground(Color.WHITE);
+		btnCancel.setFont(new Font("Roboto", Font.BOLD, 14));
+		btnCancel.setBackground(new Color(47, 79, 79));
+		btnCancel.setBounds(116, 330, 120, 35);
+		frame.getContentPane().add(btnCancel);
+		
+		JLabel GoShopperAdmin_Logo = new JLabel("New label");
+		GoShopperAdmin_Logo.setIcon(new ImageIcon(GoShopper_AdminEdit.class.getResource("/GoShopper_AdminAssets/LogoAdminSub.png")));
+		GoShopperAdmin_Logo.setBounds(-11, 0, 333, 393);
+		frame.getContentPane().add(GoShopperAdmin_Logo);
+	}
+}
