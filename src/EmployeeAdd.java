7555a2fb8e0e1c1962739e78fae299a2cd6e5d13
dumpit-ca <dumpitca@outlook.com>
2022-03-08 22:17:46 +0800

    finished Employee Add

--- src/EmployeeAdd.java
+++ src/EmployeeAdd.java
@@ -25,6 +25,8 @@ import javax.swing.UIManager;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
+import javax.swing.JPasswordField;
+import javax.swing.JCheckBox;
 
 public class EmployeeAdd {
 
@@ -41,9 +43,12 @@ public class EmployeeAdd {
      };
      private JTextField txtFName;
      private JTextField txtLName;
+     private JTextField txtUsername;
      private JTextField txtEmail;
-     private JTextField textField;
+     private JTextField txtEmpID;
+     private JPasswordField fieldPassword;
      private JTextField txtAge;
+     private JTextField txtAddress;
 
 
 	/**
@@ -75,25 +80,20 @@ public class EmployeeAdd {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 1024, 734);
+		frame.setBounds(100, 100, 718, 772);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblAge = new JLabel("Age");
-		lblAge.setFont(new Font("Dialog", Font.PLAIN, 16));
-		lblAge.setBounds(31, 411, 293, 21);
-		frame.getContentPane().add(lblAge);
+		txtUsername = new JTextField();
+		txtUsername.setBackground(new Color(245, 245, 245));
+		txtUsername.setBounds(363, 336, 294, 31);
+		frame.getContentPane().add(txtUsername);
+		txtUsername.setColumns(10);
 		
-		txtEmail = new JTextField();
-		txtEmail.setBackground(new Color(245, 245, 245));
-		txtEmail.setBounds(28, 266, 294, 31);
-		frame.getContentPane().add(txtEmail);
-		txtEmail.setColumns(10);
-		
-		JLabel lblEmail = new JLabel("Email");
-		lblEmail.setFont(new Font("Dialog", Font.ITALIC, 16));
-		lblEmail.setBounds(28, 234, 293, 21);
-		frame.getContentPane().add(lblEmail);
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("Dialog", Font.ITALIC, 16));
+		lblUsername.setBounds(363, 304, 293, 21);
+		frame.getContentPane().add(lblUsername);
 		
 		JLabel lblEmployee = new JLabel("Add Employee");
 		lblEmployee.setForeground(Color.BLACK);
@@ -114,11 +114,11 @@ public class EmployeeAdd {
 		frame.getContentPane().add(txtFName);
 		txtFName.setColumns(10);
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Last Name");
-		lblNewLabel_1_1_1.setForeground(Color.BLACK);
-		lblNewLabel_1_1_1.setFont(new Font("Segoe UI Variable", Font.ITALIC, 16));
-		lblNewLabel_1_1_1.setBounds(363, 149, 325, 21);
-		frame.getContentPane().add(lblNewLabel_1_1_1);
+		JLabel lblLName = new JLabel("Last Name");
+		lblLName.setForeground(Color.BLACK);
+		lblLName.setFont(new Font("Segoe UI Variable", Font.ITALIC, 16));
+		lblLName.setBounds(363, 149, 325, 21);
+		frame.getContentPane().add(lblLName);
 		
 		txtLName = new JTextField();
 		txtLName.setFont(new Font("Roboto", Font.PLAIN, 12));
@@ -130,21 +130,21 @@ public class EmployeeAdd {
 		JLabel lblRole = new JLabel("Role");
 		lblRole.setForeground(Color.BLACK);
 		lblRole.setFont(new Font("Segoe UI Variable", Font.ITALIC, 16));
-		lblRole.setBounds(28, 315, 325, 21);
+		lblRole.setBounds(28, 453, 293, 21);
 		frame.getContentPane().add(lblRole);
 		
 		JComboBox cmdPosition = new JComboBox();
 		cmdPosition.setFont(new Font("Dialog", Font.PLAIN, 12));
 		cmdPosition.setModel(new DefaultComboBoxModel(new String[] {"Merch", "Cashier", "Manager"}));
 		cmdPosition.setBackground(new Color(245, 245, 245));
-		cmdPosition.setBounds(28, 347, 294, 31);
+		cmdPosition.setBounds(28, 485, 294, 31);
 		frame.getContentPane().add(cmdPosition);
 		
 		JButton btnEdit = new JButton("Save");
 		btnEdit.setForeground(Color.WHITE);
 		btnEdit.setFont(new Font("Roboto", Font.BOLD, 14));
 		btnEdit.setBackground(new Color(204, 102, 102));
-		btnEdit.setBounds(34, 560, 78, 35);
+		btnEdit.setBounds(28, 653, 78, 35);
 		frame.getContentPane().add(btnEdit);
 		
 		JButton btnCancel = new JButton("Cancel");
@@ -155,42 +155,88 @@ public class EmployeeAdd {
 		btnCancel.setForeground(Color.WHITE);
 		btnCancel.setFont(new Font("Roboto", Font.BOLD, 14));
 		btnCancel.setBackground(new Color(47, 79, 79));
-		btnCancel.setBounds(122, 560, 120, 35);
+		btnCancel.setBounds(116, 653, 120, 35);
 		frame.getContentPane().add(btnCancel);
 		Image adminlogo = new ImageIcon(this.getClass().getResource("/LogoAdminSub.png")).getImage();
 		
-		JLabel lblAddress = new JLabel("Address");
-		lblAddress.setFont(new Font("Dialog", Font.ITALIC, 16));
-		lblAddress.setBounds(363, 234, 294, 21);
-		frame.getContentPane().add(lblAddress);
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setFont(new Font("Dialog", Font.ITALIC, 16));
+		lblEmail.setBounds(28, 379, 294, 21);
+		frame.getContentPane().add(lblEmail);
 		
-		textField = new JTextField();
-		textField.setBackground(new Color(245, 245, 245));
-		textField.setBounds(364, 267, 606, 31);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		txtEmail = new JTextField();
+		txtEmail.setBackground(new Color(245, 245, 245));
+		txtEmail.setBounds(28, 411, 293, 31);
+		frame.getContentPane().add(txtEmail);
+		txtEmail.setColumns(10);
 		
 		JLabel lblGender = new JLabel("Gender");
 		lblGender.setFont(new Font("Dialog", Font.ITALIC, 16));
-		lblGender.setBounds(363, 315, 294, 21);
+		lblGender.setBounds(363, 453, 294, 21);
 		frame.getContentPane().add(lblGender);
 		
 		JComboBox cmbGender = new JComboBox();
 		cmbGender.setFont(new Font("Dialog", Font.PLAIN, 12));
 		cmbGender.setModel(new DefaultComboBoxModel(new String[] {"Female", "Male"}));
-		cmbGender.setBounds(363, 347, 294, 31);
+		cmbGender.setBounds(363, 485, 294, 31);
 		frame.getContentPane().add(cmbGender);
 		
-		JLabel GoShopperAdmin_Logo = new JLabel("New label");
-		GoShopperAdmin_Logo.setIcon(new ImageIcon(adminlogo));
-		GoShopperAdmin_Logo.setBounds(-11, 0, 333, 393);
-		frame.getContentPane().add(GoShopperAdmin_Logo);
+		
+		JLabel lblEmpID = new JLabel("Employee ID");
+		lblEmpID.setFont(new Font("Dialog", Font.ITALIC, 16));
+		lblEmpID.setBounds(28, 304, 293, 21);
+		frame.getContentPane().add(lblEmpID);
+		
+		txtEmpID = new JTextField();
+		txtEmpID.setBackground(new Color(245, 245, 245));
+		txtEmpID.setBounds(28, 336, 293, 31);
+		frame.getContentPane().add(txtEmpID);
+		txtEmpID.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Dialog", Font.ITALIC, 16));
+		lblPassword.setBounds(363, 378, 294, 21);
+		frame.getContentPane().add(lblPassword);
+		
+		fieldPassword = new JPasswordField();
+		fieldPassword.setBackground(new Color(245, 245, 245));
+		fieldPassword.setBounds(363, 412, 294, 29);
+		frame.getContentPane().add(fieldPassword);
+		
+		JLabel lblUser = new JLabel("User Type");
+		lblUser.setFont(new Font("Dialog", Font.ITALIC, 16));
+		lblUser.setBounds(364, 527, 293, 21);
+		frame.getContentPane().add(lblUser);
+		
+		JCheckBox chckAdmin = new JCheckBox("Admin");
+		chckAdmin.setFont(new Font("Dialog", Font.PLAIN, 12));
+		chckAdmin.setBounds(364, 558, 97, 31);
+		frame.getContentPane().add(chckAdmin);
 		
 		txtAge = new JTextField();
 		txtAge.setBackground(new Color(245, 245, 245));
-		txtAge.setFont(new Font("Dialog", Font.PLAIN, 12));
-		txtAge.setBounds(31, 445, 291, 31);
+		txtAge.setBounds(29, 559, 294, 31);
 		frame.getContentPane().add(txtAge);
 		txtAge.setColumns(10);
+		
+		JLabel lblAge = new JLabel("Age");
+		lblAge.setFont(new Font("Dialog", Font.ITALIC, 16));
+		lblAge.setBounds(29, 527, 294, 21);
+		frame.getContentPane().add(lblAge);
+		
+		JLabel lblAddress = new JLabel("Address");
+		lblAddress.setFont(new Font("Dialog", Font.ITALIC, 16));
+		lblAddress.setBounds(28, 221, 295, 21);
+		frame.getContentPane().add(lblAddress);
+		
+		txtAddress = new JTextField();
+		txtAddress.setBounds(28, 253, 626, 31);
+		frame.getContentPane().add(txtAddress);
+		txtAddress.setColumns(10);
+		
+		JLabel GoShopperAdmin_Logo = new JLabel("New label");
+		GoShopperAdmin_Logo.setIcon(new ImageIcon(adminlogo));
+		GoShopperAdmin_Logo.setBounds(0, 0, 333, 393);
+		frame.getContentPane().add(GoShopperAdmin_Logo);
 	}
 }
